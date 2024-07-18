988df723564c3ee9a0ce07a53e92ec0827127720
Jaswantsuthar <95173617+Jaswantsuthar@users.noreply.github.com>
2024-07-18 13:07:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function SportsCar(make, model, topSpeed) {
 }
 
 SportsCar.prototype.getTopSpeed= function(){
-	return (`${this.topSpeed}`)
+	return this.topSpeed
 }
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
